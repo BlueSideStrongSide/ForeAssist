d2a9cdc3295e09c708b97cd03106adb28615ed11
BlueSideStrongSide <bmanradge@gmail.com>
2023-03-15 23:15:10 -0400

    Added additional functionality:
    Dynamic str contains filters
    Export functions for csv,json,md
    refactor export function logic

--- foreassist.py
+++ foreassist.py
@@ -5,6 +5,7 @@ import argparse
 import pandas as pd
 from pathlib import Path
 
+
 REFERENCES = ["https://www.jaiminton.com/cheatsheet/DFIR/",
               "https://github.com/ForensicArtifacts/artifacts"
               ]
@@ -66,12 +67,25 @@ def render_selected_artifacts(imported_artifacts=None, selected_artifact=None):
         if st_ready_artifact["name"] == selected_artifact:
             return st_ready_artifact
 
-@st._cache_data
-def fix_sources_view():
-    ...
+def filter_view_all_dataframe(current_dataframe:pd.DataFrame=None, _modification_container=None, _filter_checkbox=None):
 
-@st._cache_data
-def render_all_selected_artifacts(imported_artifacts=None):
+    with _modification_container:
+        #current_dataframe.columns will be used in the future, currently need to normalize list in certain coloumns
+        #using static values for now
+        if _filter_checkbox:
+            to_filter_col = st.multiselect("Filter Artifacts Data On:", ("name","doc","Sources"), default=("name"))
+            if to_filter_col:
+                for item in to_filter_col:
+                    padding_left, padding_right = st.columns((1, 100))
+                    input_left, input_right = st.columns((1, 100))
+                    wanted_substring = padding_right.text_input(f"↳ Substring found in -->  {item}:")
+                    if wanted_substring:
+                        current_dataframe = current_dataframe[current_dataframe[item].str.contains(wanted_substring, case=False)]
+
+
+    return current_dataframe
+
+def render_all_selected_artifacts(imported_artifacts:list=None, _modification_container=None, _filter_checkbox=None) -> pd.DataFrame:
     base_df = pd.DataFrame(imported_artifacts)
 
     #normalize json for nested data
@@ -83,13 +97,28 @@ def render_all_selected_artifacts(imported_artifacts=None):
 
     #need logic to add common values across all dataframes
     #ESXi does not have provides and aliases
-    # result_df = pd.concat([base_df[["name","doc","supported_os","urls","provides","aliases"]], sources_normalized], axis=1)
     result_df = pd.concat([base_df[["name", "doc", "supported_os", "urls" ]], sources_concatenated],
                           axis=1)
-    print(result_df.columns)
+
+    # print(result_df.columns)
     result_df = result_df.rename(columns={0:'Sources'})
 
-    st.dataframe(result_df)
+    #call filter function return dataframe to outter function
+    filtered_df = filter_view_all_dataframe(current_dataframe=result_df,
+                                            _modification_container=_modification_container,
+                                            _filter_checkbox=_filter_checkbox)
+    return filtered_df
+
+@st._cache_data
+def export_rendered_artifacts_by_type(input_data_frame:pd.DataFrame=None,
+                                      _export_options:dict=None,
+                                      selected_export_type:str=None):
+    extended_options=None
+    for type, export_method in _export_options.items():
+        if selected_export_type == type:
+            return export_method(input_data_frame)
+
+
 
 @st._cache_data
 def selected_artifact_stats(selected_artifacts, st_provided_stats):
@@ -104,9 +133,16 @@ def selected_artifact_stats(selected_artifacts, st_provided_stats):
     return pd.DataFrame.from_dict([artifact_stats]).astype(str).transpose()
 
 def build_streamlit_app():
+    rendered_dataframe = None
+
     #Build This Dynamically
     supported_os = ("Any", "Windows", "Linux", "Darwin", "ESXi")
-    supported_export_types = ["csv","txt","json"]
+
+    supported_exports = {"csv":pd.DataFrame.to_csv,
+            "json": pd.DataFrame.to_json,
+            "md":pd.DataFrame.to_markdown}
+
+    supported_export_types = supported_exports.keys()
 
     st.title("ForeAssist")
     st.write(f'"An OpenSource project pulling together forensic artifacts from a few community projects. The goal is to have an easy and friendly way to find relevant artifacts and supporting information on each. Currently this page uses the awesome! work of the resources listed. {" , ".join(REFERENCES)}. '
@@ -117,20 +153,6 @@ def build_streamlit_app():
 
     st.write("What OS are you interested in?")
 
-    #move this to a seperate function
-    # col1, col2, col3, col4, col5 = st.columns(5)
-    #
-    # with col1:
-    #     st.checkbox(supported_os[0])
-    # with col2:
-    #     st.checkbox(supported_os[1])
-    # with col3:
-    #     st.checkbox(supported_os[2])
-    # with col4:
-    #     st.checkbox(supported_os[3])
-    # with col5:
-    #     st.checkbox(supported_os[4])
-
     checkbox_options = {}
     for item,os in enumerate(supported_os):
             selected_os = st.checkbox(os, key=item)
@@ -138,17 +160,10 @@ def build_streamlit_app():
     st_ready_artifacts, st_ready_stats = read_supported_artifacts(wanted_os=read_selected_os_checkbox(checkbox_options))
 
     selected_artifact_visbility = "hidden"
-    # st.sidebar.subheader("Addiitional Filters")
-    # st.sidebar.radio("Modified View", ("view1", "view2", "view3"))
-    # st.sidebar.text_input("Dynamically Build Your Own Filter")
-
     st.sidebar.subheader("Collection Scripts:")
     st.sidebar.button("Collect Windows Selected Artifacts")
     st.sidebar.button("Collect Linux Selected Artifiacts")
     st.sidebar.button("Collect Artifacts Using Python")
-    st.sidebar.subheader("Export:")
-    st.sidebar.selectbox(label="Type:",options=supported_export_types)
-    st.sidebar.button("Export Filtered Artifacts")
 
     if any([x for x in checkbox_options.values()]):
         st.subheader("Included Artifacts")
@@ -160,12 +175,35 @@ def build_streamlit_app():
                     </style>
                     """
         st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
         st.table(selected_artifact_stats(st_ready_artifacts, st_ready_stats))
 
-        check_view_all = st.checkbox("View All Artifacts")
+        st.subheader("Filters")
+        view1, view2, = st.columns(2)
+        with view1:
+            check_view_all = st.checkbox("View All Artifacts")
+
         if check_view_all:
-            # need to write all matching yaml filters
-            render_all_selected_artifacts(imported_artifacts=st_ready_artifacts)
+            with view2:
+                check_additional_filters = st.checkbox("Apply Additional Filters")
+            data_modification_container = st.container()
+
+            st.subheader("Results:")
+
+            rendered_dataframe = render_all_selected_artifacts(imported_artifacts=st_ready_artifacts,
+                                          _modification_container=data_modification_container,
+                                          _filter_checkbox=check_additional_filters)
+            if not rendered_dataframe.empty:
+                st.sidebar.subheader("Export:")
+                export_type = st.sidebar.selectbox(label="Type:", options=supported_export_types)
+
+                btn_export_clicked = st.sidebar.download_button(label="Export Filtered Artifacts",
+                                                                file_name=f"filtered_exports.{export_type}",
+                                                                data=export_rendered_artifacts_by_type(
+                                                                    input_data_frame=rendered_dataframe,
+                                                                    _export_options=supported_exports,
+                                                                    selected_export_type=export_type))
+            st.dataframe(rendered_dataframe)
 
         else:
             selected_artifact_visbility = "visible"
@@ -174,8 +212,11 @@ def build_streamlit_app():
                                              label_visibility=selected_artifact_visbility)
 
             #We may need to update and pass a placeholder here
-            rendered = render_selected_artifacts(imported_artifacts=st_ready_artifacts, selected_artifact=selected_artifact)
-            st.json(rendered, expanded=True)
+            rendered_dataframe = render_selected_artifacts(imported_artifacts=st_ready_artifacts, selected_artifact=selected_artifact)
+
+            st.subheader("Results:")
+
+            st.json(rendered_dataframe, expanded=True)
 
 
 
