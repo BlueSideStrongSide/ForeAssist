15124a5b4902ea6842527f7785c05606ccf7876e
BlueSideStrongSide <bmanradge@gmail.com>
2023-05-10 20:29:28 -0400

    Add option to start expanding env variables

--- foreassist.py
+++ foreassist.py
@@ -1,4 +1,6 @@
 import json
+import re
+
 import yaml
 import streamlit as st
 import argparse
@@ -47,7 +49,6 @@ def read_supported_artifacts(wanted_os:list=None):
                         stats_by_os[os] += 1
                         parsed_artifacts.append(item)
 
-    print(stats_by_os)
     return parsed_artifacts, stats_by_os
 
 @st.cache_data
@@ -72,7 +73,7 @@ def filter_view_all_dataframe(current_dataframe:pd.DataFrame=None, _modification
     with _modification_container:
 
         if _filter_checkbox:
-            to_filter_col = st.multiselect("Filter Artifacts Data On:", ("name","doc","Sources", "supported_os", "Sources"), default=("name"))
+            to_filter_col = st.multiselect("Filter Artifacts Data On:", ("name","doc","Sources", "supported_os"), default=("name"))
             if to_filter_col:
                 for item in to_filter_col:
                     padding_left, padding_right = st.columns((1, 100))
@@ -124,6 +125,18 @@ def export_rendered_artifacts_by_type(input_data_frame:pd.DataFrame=None,_export
         if selected_export_type == type:
             return export_method(input_data_frame)
 
+#update streamlit
+@st._cache_data
+def build_env_var_expansion(input_data_frame:pd.DataFrame) -> list:
+    identified_filters = []
+    #%%environ_allusersprofile%% use regext to parse this patter return hits to set for uniq
+    temp_df = pd.DataFrame()
+    temp_df = input_data_frame["Sources"].str.extract('(%%[^,\\\]+%%)').dropna()
+
+    if not temp_df.empty:
+        identified_filters = set(temp_df[0].to_list())
+
+    return identified_filters
 
 @st._cache_data
 def selected_artifact_stats(selected_artifacts, st_provided_stats):
@@ -132,12 +145,12 @@ def selected_artifact_stats(selected_artifacts, st_provided_stats):
     # that artifact should be counted twice
 
 
-    artifact_stats = ({"Total Aqrtifacts":len(selected_artifacts),
+    artifact_stats = ({"Total Artifacts":len(selected_artifacts),
                       "Total Artifacts By OS":st_provided_stats})
 
     return pd.DataFrame.from_dict([artifact_stats]).astype(str).transpose()
 
-def build_streamlit_app():
+def build_streamlit_app() ->None:
     rendered_dataframe = None
 
     #Build This Dynamically
@@ -150,9 +163,14 @@ def build_streamlit_app():
     supported_export_types = supported_exports.keys()
 
     st.title("ForeAssist")
-    st.write(f'"An OpenSource project pulling together forensic artifacts from a few community projects. The goal is to have an easy and friendly way to find relevant artifacts and supporting information on each. Currently this page uses the awesome! work of the resources listed. {" , ".join(REFERENCES)}. '
-             f'\n\nCredit and attribution will be associated with any adhoc additions as well!')
-    st.write(f'Certain functionaility is still being added, please see the github repo for current issues/enhancements/bugs.')
+    st.write(f'"An OpenSource project pulling together forensic artifacts from a few community projects. ')
+    st.write(f'The goal is to have an easy and friendly way to find relevant artifacts and supporting information on each. ')
+    st.write(f'Currently this page uses the awesome! work of the resources listed.')
+    st.write(f'Credit and attribution will be associated with any adhoc additions as well!')
+    st.markdown("""
+    - https://www.jaiminton.com/cheatsheet/DFIR/
+    - https://github.com/ForensicArtifacts/artifacts
+    """)
 
     st.sidebar.file_uploader(
         "Give me a list of available filenames, I will check if any match a LIKELY forensic artifact")
@@ -192,6 +210,7 @@ def build_streamlit_app():
         if check_view_all:
             with view2:
                 check_additional_filters = st.checkbox("Apply Additional Filters")
+
             data_modification_container = st.container()
 
             st.subheader("Results:")
@@ -199,7 +218,16 @@ def build_streamlit_app():
             rendered_dataframe = render_all_selected_artifacts(imported_artifacts=st_ready_artifacts,
                                           _modification_container=data_modification_container,
                                           _filter_checkbox=check_additional_filters)
+
             if not rendered_dataframe.empty:
+                wanted_expansion = build_env_var_expansion(input_data_frame=rendered_dataframe)
+                if wanted_expansion:
+                    st.sidebar.subheader("Environment Variables")
+                    st.sidebar.write("Use the section below to expand provided environment variables.")
+                    for item in wanted_expansion:
+                        st.sidebar.text_input(item, "")
+
+
                 st.sidebar.subheader("Export:")
                 export_type = st.sidebar.selectbox(label="Type:", options=supported_export_types)
 
@@ -213,6 +241,7 @@ def build_streamlit_app():
 
         else:
             selected_artifact_visbility = "visible"
+
             #build select box of artifacts macthing the OS filter
             selected_artifact = st.selectbox("Included Artifacts", [st_ready_label['name'] for st_ready_label in st_ready_artifacts],
                                              label_visibility=selected_artifact_visbility)
@@ -221,9 +250,10 @@ def build_streamlit_app():
             rendered_dataframe = render_selected_artifacts(imported_artifacts=st_ready_artifacts, selected_artifact=selected_artifact)
 
             st.subheader("Results:")
-
             st.json(rendered_dataframe, expanded=True)
 
+    st.write(f'Certain functionaility is still being added, please see the github repo for current issues/enhancements/bugs.'
+             f' https://github.com/BlueSideStrongSide/ForeAssist/issues.')
 
 
 if __name__ == '__main__':
